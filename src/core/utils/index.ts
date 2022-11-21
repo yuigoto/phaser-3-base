486d1ee21ecc71d14c1a2a2d517e0b07671f2701
Fabio Y. Goto <lab@yuiti.dev>
2022-11-21 02:22:11 -0300

    Added utilities and types

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -28,3 +28,59 @@ export function onDocumentReady(callback: (event: Event) => void) {
     }
   }
 }
+
+/**
+ * Guarantees that `property` exists in `value`.
+ *
+ * @param value
+ *     Value to check
+ * @param property
+ *     Property to check
+ * @returns
+ */
+export function hasOwn(value: any, property: string): boolean {
+  return value && Object.prototype.hasOwnProperty.call(value, property);
+}
+
+/**
+ * Describes a log message, when grouping logs.
+ */
+type LoggerConfig = {
+  /**
+   * Location where the log is coming from.
+   */
+  location: string;
+
+  /**
+   * Message to log.
+   */
+  message?: string;
+
+  /**
+   * Callable with additional data for logging.
+   */
+  additionalData?: () => any | void;
+};
+
+/**
+ * Logs a message to the console.
+ *
+ * @param data
+ *     Data to log
+ * @param config
+ *     Optional configuration for the log
+ */
+export function logger(data: any, config: LoggerConfig | null = null) {
+  if (config) {
+    console.groupCollapsed(
+      `[${config.location}]${': ' + config.message || ''}`
+    );
+    console.log(data);
+    if (config.additionalData && config.additionalData instanceof Function) {
+      console.log(config.additionalData());
+    }
+    console.groupEnd();
+  } else {
+    console.log(data);
+  }
+}
